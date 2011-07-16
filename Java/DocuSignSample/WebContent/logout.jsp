8201ee53f2b064aa0297c3bab35f68af09385dac
Mike Borozdin <nolongemails@gmail.com>
2011-07-15 17:00:26 -0700

    resetting some pages so Nick can work on some javascript

--- Java/DocuSignSample/WebContent/logout.jsp
+++
@@ -1,9 +0,0 @@
-<%session.invalidate();%>
-<html>
-    <head>
-        <meta http-equiv="REFRESH" content="0;url=index.jsp">
-    </head>
-    <body>
-        <p>You shouldn't see this for more than fraction of a second, because all this page does is clear your session and send you back to the login page.</p>
-    </body>
-+</html>
--- Java/DocuSignSample/src/net/docusign/sample/Login.java
+++ Java/DocuSignSample/src/net/docusign/sample/Login.java
@@ -16,8 +16,7 @@ import net.docusign.credential.LoginResponse.LoginResult;
  * Servlet implementation class Login
  */
 public class Login extends HttpServlet {
-
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -25,93 +24,101 @@ public class Login extends HttpServlet {
     public Login() {
         super();
     }
+    
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        if (session.getAttribute(Utils.SESSION_LOGGEDIN) != null
-                && session.getAttribute(Utils.SESSION_LOGGEDIN).equals(true)) {
-            response.sendRedirect(Utils.CONTROLLER_SENDDOCUMENT);
-        } else {
-            session.setAttribute(Utils.SESSION_ENVELOPEIDS, null);
-            session.setAttribute(Utils.SESSION_ERROR_MSG, null);
-            session.setAttribute(Utils.SESSION_LOGGEDIN, false);
-            Properties creds = new Properties();
-            try {
-                creds.load(getClass().getResourceAsStream(
-                        Utils.CONFIG_PROPERTIES_CLASSPATH_LOCATION));
-                session.setAttribute(Utils.SESSION_EMAIL, creds.getProperty(Utils.SESSION_EMAIL));
-                session.setAttribute(Utils.SESSION_PASSWORD, creds.getProperty(Utils.SESSION_PASSWORD));
-                session.setAttribute(Utils.SESSION_INTEGRATORS_KEY, creds.getProperty(Utils.SESSION_INTEGRATORS_KEY));
-                session.setAttribute(Utils.DOCUSIGN_CREDENTIAL_ENDPOINT, creds.getProperty(Utils.DOCUSIGN_CREDENTIAL_ENDPOINT));
-                session.setAttribute(Utils.DOCUSIGN_WEBSERVICE_ENDPOINT, creds.getProperty(Utils.DOCUSIGN_WEBSERVICE_ENDPOINT));
-            } catch (IOException ioexception) {
-                System.err.println(Utils.ERROR_CONFIG);
-            }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	HttpSession session = request.getSession();
+		if (session.getAttribute(Utils.SESSION_LOGGEDIN) != null &&
+				session.getAttribute(Utils.SESSION_LOGGEDIN).equals(true)) {
+				response.sendRedirect(Utils.CONTROLLER_SENDDOCUMENT);
+		}
+		else {
+			session.setAttribute(Utils.SESSION_ENVELOPEIDS, null);
+			session.setAttribute(Utils.SESSION_ERROR_MSG, null);
+			session.setAttribute(Utils.SESSION_LOGGEDIN, false);
+			Properties creds = new Properties();
+			try {
+				creds.load(getClass().getResourceAsStream(
+						Utils.CONFIG_PROPERTIES_CLASSPATH_LOCATION));
+				session.setAttribute(Utils.SESSION_EMAIL, creds.getProperty(Utils.SESSION_EMAIL));
+				session.setAttribute(Utils.SESSION_PASSWORD, creds.getProperty(Utils.SESSION_PASSWORD));
+				session.setAttribute(Utils.SESSION_INTEGRATORS_KEY, creds.getProperty(Utils.SESSION_INTEGRATORS_KEY));
+				session.setAttribute(Utils.DOCUSIGN_CREDENTIAL_ENDPOINT, creds.getProperty(Utils.DOCUSIGN_CREDENTIAL_ENDPOINT));
+				session.setAttribute(Utils.DOCUSIGN_WEBSERVICE_ENDPOINT, creds.getProperty(Utils.DOCUSIGN_WEBSERVICE_ENDPOINT));
+			} catch (IOException ioexception) {
+				System.err.println(Utils.ERROR_CONFIG);
+			}
+	
+	    	response.sendRedirect(Utils.PAGE_LOGIN);
+		}
+	}
 
-            response.sendRedirect(Utils.PAGE_LOGIN);
-        }
-    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		// Handle Submit button
+		if (request.getParameterValues(Utils.NAME_SUBMIT) != null) {
+			session.setAttribute(Utils.SESSION_EMAIL, request.getParameterValues(Utils.NAME_EMAIL)[0]);
+			session.setAttribute(Utils.SESSION_PASSWORD, request.getParameterValues(Utils.NAME_PASSWORD)[0]);
+			session.setAttribute(Utils.SESSION_INTEGRATORS_KEY, request.getParameterValues(Utils.NAME_IKEY)[0]);
+			if (login(request, response)) {
+				session.setAttribute(Utils.SESSION_LOGGEDIN, true);
+				response.sendRedirect(Utils.CONTROLLER_SENDDOCUMENT);
+			}
+			else {
+				session.setAttribute(Utils.SESSION_LOGGEDIN, false);
+				response.sendRedirect(Utils.PAGE_ERROR);
+			}
+		}
+		
+		// Handle Reset button
+		if (request.getParameterValues(Utils.NAME_RESET) != null) {
+			doGet(request, response);
+		}
+	}
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
 
-        // Handle Submit button
-        if (request.getParameterValues(Utils.NAME_SUBMIT) != null) {
-            session.setAttribute(Utils.SESSION_EMAIL, request.getParameterValues(Utils.NAME_EMAIL)[0]);
-            session.setAttribute(Utils.SESSION_PASSWORD, request.getParameterValues(Utils.NAME_PASSWORD)[0]);
-            session.setAttribute(Utils.SESSION_INTEGRATORS_KEY, request.getParameterValues(Utils.NAME_IKEY)[0]);
-            if (login(request, response)) {
-                session.setAttribute(Utils.SESSION_LOGGEDIN, true);
-                response.sendRedirect(Utils.CONTROLLER_SENDDOCUMENT);
-            } else {
-                session.setAttribute(Utils.SESSION_LOGGEDIN, false);
-                response.sendRedirect(Utils.PAGE_ERROR);
-            }
-        }
-
-        // Handle Reset button
-        if (request.getParameterValues(Utils.NAME_RESET) != null) {
-            doGet(request, response);
-        }
-    }
-
-    private boolean login(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        boolean ret = false;
-        LoginResult result = null;
-        HttpSession session = request.getSession();
-        session.setAttribute(Utils.SESSION_ERROR_MSG, null);
-
-        CredentialSoap credApi = new CredentialFactory().getCredential(session.getAttribute(Utils.DOCUSIGN_CREDENTIAL_ENDPOINT).toString());
-
-        try {
-            // add integrator key TODO: remove if this is changed
-            result = credApi.login(
-                    "[" + session.getAttribute(Utils.SESSION_INTEGRATORS_KEY).toString() + "]"
-                    + session.getAttribute(Utils.SESSION_EMAIL).toString(),
-                    session.getAttribute(Utils.SESSION_PASSWORD).toString());
-        } catch (Exception e) {
-            // TODO: handle exception
-            ret = false;
-        }
-
-        if (result != null && result.isSuccess()) {
-            session.setAttribute(Utils.SESSION_ACCOUNT_ID,
-                    result.getAccounts().getAccount().get(0).getAccountID());
-            session.setAttribute(Utils.SESSION_USER_ID,
-                    result.getAccounts().getAccount().get(0).getUserID());
-            ret = true;
-        } else {
-            session.setAttribute(Utils.SESSION_ERROR_MSG, result.getErrorCode());
-            ret = false;
-        }
-
-        return ret;
-    }
+	private boolean login(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		boolean ret = false;
+		LoginResult result = null;
+		HttpSession session = request.getSession();
+		session.setAttribute(Utils.SESSION_ERROR_MSG, null);
+		
+		CredentialSoap credApi = new CredentialFactory().getCredential(
+				session.getAttribute(Utils.DOCUSIGN_CREDENTIAL_ENDPOINT).toString());
+		
+		try {
+			// add integrator key TODO: remove if this is changed
+			result = credApi.login(
+					"[" + session.getAttribute(Utils.SESSION_INTEGRATORS_KEY).toString() + "]" +
+					session.getAttribute(Utils.SESSION_EMAIL).toString(), 
+					session.getAttribute(Utils.SESSION_PASSWORD).toString());
+		} catch (Exception e) {
+			// TODO: handle exception
+			ret = false;
+		}
+		
+		if (result != null && result.isSuccess()) {
+			session.setAttribute(Utils.SESSION_ACCOUNT_ID, 
+					result.getAccounts().getAccount().get(0).getAccountID());
+			session.setAttribute(Utils.SESSION_USER_ID, 
+					result.getAccounts().getAccount().get(0).getUserID());
+			ret = true;
+		}
+		else {
+			session.setAttribute(Utils.SESSION_ERROR_MSG, result.getErrorCode());
+			ret = false;
+		}
+		
+		return ret;
+	}
+		
 }
