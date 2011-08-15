cce1472cc28e8c9b15e76739a584cd665a78c9a5
Jeffrey Kyllo <jeff.kyllo@docusign.com>
2011-08-15 09:31:05 -0700

    Added support for multiple accounts.

---
+++ MS.NET/DocuSignSample/DocuSignSample/SelectUser.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DocuSignSample
+{
+    public partial class SelectUser : BasePage
+    {
+        protected IEnumerable<String> names;
+        protected IDictionary<String, CredentialAPI.Account> accounts;
+
+        protected string email;
+        protected string password;
+        protected string key;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                OnSelect();
+            }
+            else
+            {
+                if (Session["APIAccounts"] != null &&
+                    Session["APIEmail"] != null &&
+                    Session["APIPassword"] != null &&
+                    Session["APIIKey"] != null)
+                {
+                    names = ((IDictionary<String, CredentialAPI.Account>)Session["APIAccounts"]).Keys;
+                    accounts = ((IDictionary<String, CredentialAPI.Account>)Session["APIAccounts"]);
+                    email = Session["APIEmail"].ToString();
+                    password = Session["APIPassword"].ToString();
+                    key = Session["APIIKey"].ToString();
+                }
+                else
+                {
+                    GoToErrorPage("Could not log you in. Please check your credentials.");
+                }
+            }
+        }
+
+        private void OnSelect()
+        {
+            String selectedName = Request.Form["DevCenterName"];
+            IDictionary<String, CredentialAPI.Account> accounts = (IDictionary<String, CredentialAPI.Account>)Session["APIAccounts"];
+            if(accounts.ContainsKey(selectedName)) {
+                Session["APIAccountId"] = accounts[selectedName].AccountID;
+                Session["APIUserID"] = accounts[selectedName].UserID;
+                Response.Redirect("SendDocument.aspx", true);
+            } else {
+                GoToErrorPage("Could not log you in. Please check your credentials.");
+            }
+        }
+    }
+}
