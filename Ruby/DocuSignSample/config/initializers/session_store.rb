b90dc48f435ea540053b857f14ee4b78e16a37ee
P Prafulla Kiran <prafulla.kiran@gmail.com>
2013-07-29 12:03:40 +0530

    Fixing various issues with Ruby version of the samples

--- Ruby/DocuSignSample/config/initializers/session_store.rb
+++ Ruby/DocuSignSample/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DocuSignSample::Application.config.session_store :cookie_store, :key => '_DocuSignSample_session'
+DocuSignSample::Application.config.session_store :active_record_store, :key => '_DocuSignSample_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
