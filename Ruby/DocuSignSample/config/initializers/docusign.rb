f2191422525b76fb0d33abe1f6fd679331c7798d
P Prafulla Kiran <prafulla.kiran@gmail.com>
2013-08-08 12:41:16 +0530

    Email field should accept only email addresses

--- Ruby/DocuSignSample/config/initializers/docusign.rb
+++ Ruby/DocuSignSample/config/initializers/docusign.rb
@@ -4,4 +4,4 @@ require 'docusign'
 
 Docusign::Config = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/docusign.yml")[Rails.env])
 Docusign::Config[:verify_mode] = OpenSSL::SSL::VERIFY_PEER
-Docusign::Config[:ca_file] = '/usr/lib/ssl/certs/ca-certificates.crt'
+Docusign::Config[:ca_file] = '/usr/local/share/ca-bundle.crt'
