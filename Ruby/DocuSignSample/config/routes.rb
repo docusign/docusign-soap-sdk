5e681c58b57e8e7ad8018e54936a3e691577c8cd
Naveen Gopala <gsnavin@gmail.com>
2018-12-10 16:40:00 -0800

    Updated to rails v2.4.5

--- Ruby/DocuSignSample/config/routes.rb
+++ Ruby/DocuSignSample/config/routes.rb
@@ -15,39 +15,40 @@
 # PARTICULAR PURPOSE.
 #
 
-DocuSignSample::Application.routes.draw do
+Rails.application.routes.draw do
+# DocuSignSample::Application.routes.draw do
   root :to => "logins", :action => "new"
 
   resources :application, :only => [:clear_session]
-  match "application/clear_session" => "application#clear_session"
+  get "application/clear_session" => "application#clear_session"
 
   resources :logins, :only => [:new, :create]
-  match "logins/create" => "logins#create"
+  post "logins/create" => "logins#create"
 
   resources :send_document, :only => [:create, :send_envelope, :logout]
-  match "send_document" => "send_document#create"
-  match "send_document/send_envelope" => "send_document#send_envelope"
-  match "send_document/logout" => "send_document#logout"
+  get "send_document" => "send_document#create"
+  post "send_document/send_envelope" => "send_document#send_envelope"
+  post "send_document/logout" => "send_document#logout"
 
   resources :send_template, :only => [:create, :edit, :logout]
-  match "send_template/" => "send_template#create"
-  match "send_template/send_template" =>"send_template#send_template"
-  match "send_template/get_template" => "send_template#get_template"
-  match "send_template/edit" => "send_template#edit"
-  match "send_template/logout" => "send_template#logout"
+  get "send_template/" => "send_template#create"
+  post "send_template/send_template" =>"send_template#send_template"
+  post "send_template/get_template" => "send_template#get_template"
+  post "send_template/edit" => "send_template#edit"
+  post "send_template/logout" => "send_template#logout"
 
   resources:embed_docusign, :only => [:new, :edit, :logout]
-  match "embed_docusign/" => "embed_docusign#new"
-  match "embed_docusign/create" =>"embed_docusign#create"
-  match "embed_docusign/logout" => "embed_docusign#logout"
+  get "embed_docusign/" => "embed_docusign#new"
+  post "embed_docusign/create" =>"embed_docusign#create"
+  post "embed_docusign/logout" => "embed_docusign#logout"
 
   resources :get_status_and_docs, :only => [:list, :show, :download, :logout]
-  match "get_status_and_docs/" => "get_status_and_docs#list"
-  match "get_status_and_docs/show" => "get_status_and_docs#show"
-  match "get_status_and_docs/logout" => "get_status_and_docs#logout"
+  get "get_status_and_docs/" => "get_status_and_docs#list"
+  post "get_status_and_docs/show" => "get_status_and_docs#show"
+  post "get_status_and_docs/logout" => "get_status_and_docs#logout"
 
   resources :error, :only => [:show, :logout]
-  match "error/" => "error#show"
-  match "error/show" => "error#show"
-  match "error/logout" => "error#logout"
+  get "error/" => "error#show"
+  get "error/show" => "error#show"
+  post "error/logout" => "error#logout"
 end
